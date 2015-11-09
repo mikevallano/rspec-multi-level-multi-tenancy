124453bff39b2504412e480e918c206ada5110b5
mike vallano <mikevallano@gmail.com>
2015-11-09 17:18:19 -0600

    Locked down pages controller, and added links for invites

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:account_home, :welcome]
+
   def home
   end
 
   def about
+  end
+
+  def account_home
+  end
+
+  def welcome
   end
 end
